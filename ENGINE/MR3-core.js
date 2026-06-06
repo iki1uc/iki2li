9034c0cb391c3939ec17488dbce6f5a616e1ffec
iki1uc <entertrainer@web.de>
2026-06-06 19:34:51 +0200

    Update MR3-core.js

--- ENGINE/MR3-core.js
+++ ENGINE/MR3-core.js
@@ -1,1 +1,13 @@
+// MR3-CORE
+// Minimaler Kern für MR³-Kompatibilität
+
+const MR3 = {
+  id: "MR3-CORE",
+  active: true,
+  version: "EVO2",
+
+  ping() {
+    return "MR3-CORE bereit";
+  }
+};
 
