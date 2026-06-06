68bb9810f27ffd2179f07d0577d5f86acb36b6fa
iki1uc <entertrainer@web.de>
2026-06-06 19:35:10 +0200

    Update GWC-engine.js

--- ENGINE/GWC-engine.js
+++ ENGINE/GWC-engine.js
@@ -1,1 +1,17 @@
+// GWC-ENGINE
+// Geo-Width-Control für MINI/NORMAL/MAXI
+
+const GWC = {
+  id: "GWC-ENGINE",
+  active: true,
+
+  scale(mode) {
+    const map = {
+      mini: 0.8,
+      normal: 1.0,
+      maxi: 1.2
+    };
+    return map[mode] || 1.0;
+  }
+};
 
