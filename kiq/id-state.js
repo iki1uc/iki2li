68bb7f650a67ef5ecc4c5cead2c888bd44e8839b
iki1uc <entertrainer@web.de>
2026-06-08 16:46:56 +0200

    Update id-state.js

--- kiq/id-state.js
+++ kiq/id-state.js
@@ -1,1 +1,10 @@
-id-state.js
+// id-state.js – liefert ID-Daten für index.html
+
+export function idState() {
+    return {
+        id: window.KIQ?.ID?.value || "UNBEKANNT",
+        axis: window.KIQ?.ID?.axis || 0,
+        instance: window.KIQ?.ID?.instance || 0,
+        base: window.KIQ?.ID?.base || 0
+    };
+}
