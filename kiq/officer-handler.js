b52a155891312eaf11ee4bc21daff90fc1e7a128
iki1uc <entertrainer@web.de>
2026-06-08 16:34:42 +0200

    Update officer-handler.js

--- kiq/officer-handler.js
+++ kiq/officer-handler.js
@@ -1,1 +1,22 @@
+// officer-handler.js – OFFICER Kontroll-Handler
 
+import { FUSION_VALIDATOR } from "./fusion-validator.js";
+import { FUSION_OPERATOR } from "./fusion-operator.js";
+
+export const OFFICER_HANDLER = {
+    id: "OFFICER-HANDLER",
+    version: "1.0",
+    created: "2026-06-08",
+
+    check(scan, tool) {
+        const fusion = FUSION_OPERATOR.fuse(scan, tool);
+        const validation = FUSION_VALIDATOR.validate(scan, tool, () => true);
+
+        return {
+            officer: true,
+            fusion,
+            validation,
+            timestamp: Date.now()
+        };
+    }
+};
