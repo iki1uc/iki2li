dd03cdf150a8f4bcff8f1d4061753227d03a69bc
iki1uc <entertrainer@web.de>
2026-06-08 16:40:02 +0200

    Update triangle-prep.js

--- kiq/triangle-prep.js
+++ kiq/triangle-prep.js
@@ -1,1 +1,10 @@
+// triangle-prep.js – liefert 2 vorbereitete Winkel für index.html
+
+export function trianglePrep() {
+    return {
+        w1: window.KIQ?.WINKEL?.A || 0,   // erster Winkel
+        w2: window.KIQ?.WINKEL?.B || 0,   // zweiter Winkel
+        valid: (window.KIQ?.WINKEL?.A + window.KIQ?.WINKEL?.B) < 180
+    };
+}
 
