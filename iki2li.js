79aed287dbc1a5adc9a7c3d885ebf51bed376ec9
iki1uc <entertrainer@web.de>
2026-07-23 18:02:55 +0200

    Update iki2li.js

--- iki2li.js
+++ iki2li.js
@@ -1,8 +1,3 @@
-import { os } from "./core/os.js";
-import { iki2li } from "./core/iki2li-native.js";
-import { PQ } from "./core/PQ-runner.js";
+import { runOdamirt } from "./iki2li-executor-01.js";
 
-const value = os.load();
-const pulse = iki2li.pulse(value);
-
-PQ.start(pulse);
+window.iki2li.odamirt = runOdamirt();
